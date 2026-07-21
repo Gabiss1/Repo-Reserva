081002870fd46368cb3dba7643003c43b5351c2d
Lucca Fagundes <luccapfagundes@gmail.com>
2026-07-20 21:50:58 -0300

    Pagina principal atualizada

--- Fontes/frontend/src/ButtonAppBar.tsx
+++ Fontes/frontend/src/ButtonAppBar.tsx
@@ -1,14 +1,31 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+
+import { Drawer } from "./components/Drawer";
 
 export default function ButtonAppBar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
+      <Drawer
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <h2>Menu</h2>
+
+        <ul>
+          <li>Home</li>
+          <li>Perfil</li>
+          <li>Configurações</li>
+        </ul>
+      </Drawer>
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -17,15 +34,20 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={() => setOpen(true)}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
             News
           </Typography>
+
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+
+      
     </Box>
   );
 }
